57200224262ca0692c17fcafe63f120340485ded
hanamon <devparkhana@gmail.com>
2021-08-24 10:39:30 +0900

    [UPDATE] custom CSS and JS

--- js/custom.js
+++ js/custom.js
@@ -2,30 +2,28 @@ jQuery(function(){ // custom.js
 	customOBJ.init();
 });
 
-var $ = jQuery;
-
 var customOBJ = {
 	init: function(){
 		customOBJ.headerEvent();
 	},
 	headerEvent: function(){
 		// Mobile 메뉴 (열기)
-		$('a.mobileAppbarBtn').on({
+		jQuery('a.mobileAppbarBtn').on({
 			click: function(){
-				$("html").addClass("addHtmlScroll");
-				$(".mobile-side-menu-container").stop().show(0);
-				$(".mobile-side-menu-container .mobile-side-bg-wrap").stop().animate({ opacity:0 }, 0).animate({ opacity:1 }, 200);
-				$(".mobile-side-menu-container .mobile-side-menu-wrap").stop().animate({ right:'-100%' }, 0).animate({ right:0 }, 200);
+				jQuery("html").addClass("addHtmlScroll");
+				jQuery(".mobile-side-menu-container").stop().show(0);
+				jQuery(".mobile-side-menu-container .mobile-side-bg-wrap").stop().animate({ opacity:0 }, 0).animate({ opacity:1 }, 200);
+				jQuery(".mobile-side-menu-container .mobile-side-menu-wrap").stop().animate({ right:'-100%' }, 0).animate({ right:0 }, 200);
 			}
 		});
 		
 		// Mobile 메뉴 (닫기) : 배경 & 버튼
-		$(".mobile-side-menu-container .mobile-side-bg-wrap").on({
+		jQuery(".mobile-side-menu-container .mobile-side-bg-wrap").on({
 			click: function(){
-				$("html").removeClass("addHtmlScroll");
-				$(".mobile-side-menu-container .mobile-side-menu-wrap").stop().animate({ right:0 }, 0).animate({ right:'-100%' }, 400);
-				$(".mobile-side-menu-container .mobile-side-bg-wrap").stop().animate({ opacity:1 }, 0).animate({ opacity:0 }, 400, function(){
-					$(".mobile-side-menu-container").stop().hide(0);
+				jQuery("html").removeClass("addHtmlScroll");
+				jQuery(".mobile-side-menu-container .mobile-side-menu-wrap").stop().animate({ right:0 }, 0).animate({ right:'-100%' }, 400);
+				jQuery(".mobile-side-menu-container .mobile-side-bg-wrap").stop().animate({ opacity:1 }, 0).animate({ opacity:0 }, 400, function(){
+					jQuery(".mobile-side-menu-container").stop().hide(0);
 				});
 			}
 		});
